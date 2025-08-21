4e556a6e0b1b391be00dc1b63f0cf565b855c8d1
Kazuo Nakanishi <nakanishi.kazuo@hmw.gr.jp>
2025-08-21 10:07:28 +0900

    PRレビューテスト1

--- CopilotReviewTest/Program.vb
+++ CopilotReviewTest/Program.vb
@@ -7,5 +7,9 @@ Module Program
         Dim number As Integer = 42
         Dim message As String = "The answer to life, the universe, and everything is " & number.ToString()
         Console.WriteLine(message)
+        Dim numbers As Integer() = {1, 2, 3, 4, 5}
+        For Each num As Integer In numbers
+            Console.WriteLine("Number: " & num)
+        Next
     End Sub
 End Module
